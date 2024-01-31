1ead1ea2ac2ff40dd575d5bcf3f962f048e4ec52
Lucas Bitencourt <lblukz@gmail.com>
2024-01-31 18:10:28 -0300

    update game score system

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import "./App.css";
 const App = () => {
   const [player1Point, setPlayer1Point] = useState("0");
   const [player2Point, setPlayer2Point] = useState("0");
+  const [player1Game, setPlayer1Game] = useState(0);
+  const [player2Game, setPlayer2Game] = useState(0);
+  const [player1Set, setPlayer1Set] = useState(0);
+  const [player2Set, setPlayer2Set] = useState(0);
   const [player1Name, setPlayer1Name] = useState("Player 1");
   const [player2Name, setPlayer2Name] = useState("Player 2");
 
@@ -32,14 +36,12 @@ const App = () => {
           }
           setPlayer1Point("0");
           setPlayer2Point("0");
+          updateGame(1);
           break;
         case "A":
           setPlayer1Point("0");
           setPlayer2Point("0");
-          break;
-        case " ":
-          setPlayer1Point("40");
-          setPlayer2Point("40");
+          updateGame(1);
           break;
         default:
           setPlayer1Point("error");
@@ -70,18 +72,59 @@ const App = () => {
           }
           setPlayer1Point("0");
           setPlayer2Point("0");
+          updateGame(2);
           break;
         case "A":
           setPlayer1Point("0");
           setPlayer2Point("0");
-          break;
-        case " ":
-          setPlayer1Point("40");
-          setPlayer2Point("40");
+          updateGame(2);
           break;
         default:
           setPlayer2Point("error");
           break;
+      }
+    }
+  };
+
+  const updateGame = (player) => {
+    if (player === 1) {
+      score1: {
+        if (player1Game === 5) {
+          if (player2Game >= 5) setPlayer1Game(6);
+          else {
+            setPlayer1Game(0);
+            setPlayer2Game(0);
+            setPlayer1Set(player1Set + 1);
+            break score1;
+          }
+        }
+        if (player1Game === 6) {
+          setPlayer1Game(0);
+          setPlayer2Game(0);
+          setPlayer1Set(player1Set + 1);
+          break score1;
+        }
+        setPlayer1Game(player1Game + 1);
+      }
+    }
+    if (player === 2) {
+      score2: {
+        if (player2Game === 5) {
+          if (player1Game >= 5) setPlayer2Game(6);
+          else {
+            setPlayer1Game(0);
+            setPlayer2Game(0);
+            setPlayer2Set(player2Set + 1);
+            break score2;
+          }
+        }
+        if (player2Game === 6) {
+          setPlayer1Game(0);
+          setPlayer2Game(0);
+          setPlayer2Set(player2Set + 1);
+          break score2;
+        }
+        setPlayer2Game(player2Game + 1);
       }
     }
   };
@@ -92,17 +135,23 @@ const App = () => {
       <div className="score-container">
         <div className="player-container">
           <h2>{player1Name}</h2>
-          <h3>{player1Point}</h3>
+          <h3>{`Games: ${player1Game}`}</h3>
+          <h3>{`Points: ${player1Point}`}</h3>
           <button onClick={() => updatePoint(1)}>Add Point</button>
           <br />
-          <button onClick={() => setPlayer1Point("0")}>Reset game score</button>
+          <button onClick={() => setPlayer1Point("0")}>Reset point score</button>
+          <br />
+          <button onClick={() => setPlayer1Game(0)}>Reset game score</button>
         </div>
         <div className="player-container">
           <h2>{player2Name}</h2>
-          <h3>{player2Point}</h3>
+          <h3>{`Games: ${player2Game}`}</h3>
+          <h3>{`Points: ${player2Point}`}</h3>
           <button onClick={() => updatePoint(2)}>Add Point</button>
           <br />
-          <button onClick={() => setPlayer2Point("0")}>Reset game score</button>
+          <button onClick={() => setPlayer2Point("0")}>Reset point score</button>
+          <br />
+          <button onClick={() => setPlayer1Game(0)}>Reset game score</button>
         </div>
       </div>
     </div>
